4d163347202f414c3e80ace5983cf5ef3a3c3696
mwiepert <mwiepert@bioperl.org>
2002-09-24 16:06:41 +0000

    Update SYNOPSIS code so that it runs
    svn path=/bioperl-run/trunk/; revision=13214

--- Bio/Tools/Run/RemoteBlast.pm
+++ Bio/Tools/Run/RemoteBlast.pm
@@ -21,51 +21,72 @@ Remote-blast "factory object" creation and blast-parameter initialization:
 
   use Bio::Tools::Run::RemoteBlast;
   use strict;
-  my $v = 1;
   my $prog = 'blastp';
   my $db   = 'swissprot';
   my $e_val= '1e-10';
 
   my @params = ( '-prog' => $prog,
-  		 '-data' => $db,
-  		 '-expect' => $e_val );
-                 '-readmethod' => 'SearchIO' );
+         '-data' => $db,
+         '-expect' => $e_val, 
+         '-readmethod' => 'SearchIO' );
 
   my $factory = Bio::Tools::Run::RemoteBlast->new(@params);
-  $v = 1;
+  
+  #change a paramter
+  $Bio::Tools::Run::RemoteBlast::HEADER{'ENTREZ_QUERY'} = 'Homo sapiens [ORGN]';
+  
+  #remove a parameter
+  delete $Bio::Tools::Run::RemoteBlast::HEADER{'FILTER'};
+  
+  my $v = 1;
   my $str = Bio::SeqIO->new(-file=>'amino.fa' , '-format' => 'fasta' );
-  my $input = $str->next_seq();
-  #  Blast a sequence against a database:
-  my $r = $factory->submit_blast($input);
-  print STDERR "waiting..." if( $v > 0 );
-  while ( my @rids = $factory->each_rid ) {
-      foreach my $rid ( @rids ) {
-  	  my $rc = $factory->retrieve_blast($rid);
-  	  if( !ref($rc) ) {
-  	      if( $rc < 0 ) {
-  		      $factory->remove_rid($rid);
-  		  }
-  	      print STDERR "." if ( $v > 0 );
-  	      sleep 5;
-  	  } else { 
-          #save the output
-          my $filename = $result->query_accession()."\.out";
-          $factory->save_output($filename);
-  	      $factory->remove_rid($rid);
-  	      while ( my $hit = $rc->nextSbjct ) {
-  		  next unless ( $v > 0);
-  		  print "hit name is ", $hit->name, "\n";
-  		  while( my $hsp = $hit->nextHSP ) {
-  		      print "score is ", $hsp->score, "\n";
-  		  } 
-  	      }
-  	  }
-      }
-  }
 
+  while (my $input = $str->next_seq()){
+    #Blast a sequence against a database:
+    
+    #Alternatively, you could  pass in a file with many  
+    #sequences rather than loop through sequence one at a time
+    #Remove the loop starting 'while (my $input = $str->next_seq())'
+    #and swap the two lines below for an example of that.
+    my $r = $factory->submit_blast($input);
+    #my $r = $factory->submit_blast('amino.fa');
+
+    print STDERR "waiting..." if( $v > 0 );
+    while ( my @rids = $factory->each_rid ) {
+      foreach my $rid ( @rids ) {
+        my $rc = $factory->retrieve_blast($rid);
+        if( !ref($rc) ) {
+          if( $rc < 0 ) {
+            $factory->remove_rid($rid);
+          }
+          print STDERR "." if ( $v > 0 );
+          sleep 5;
+        } else { 
+          my $result = $rc->next_result();
+          #save the output
+          my $filename = $result->query_name()."\.out";
+          $factory->save_output($filename);
+          $factory->remove_rid($rid);
+          print "\nQuery Name: ", $result->query_name(), "\n";
+          while ( my $hit = $result->next_hit ) {
+            next unless ( $v > 0);
+            print "\thit name is ", $hit->name, "\n";
+            while( my $hsp = $hit->next_hsp ) {
+              print "\t\tscore is ", $hsp->score, "\n";
+            } 
+          }
+        }
+      }
+    }
+  }
+  
 This example shows how to change a CGI parameter:
 
 $Bio::Tools::Run::RemoteBlast::HEADER{'MATRIX_NAME'} = 'BLOSUM25';
+
+And this is how to delete a CGI parameter:
+
+delete $Bio::Tools::Run::RemoteBlast::HEADER{'FILTER'};
 
 For a description of the many CGI parameters see:
 
