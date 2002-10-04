493f87512144ba4719317081c4ec02f356f60d0a
savikalpa <savikalpa@bioperl.org>
2002-10-04 08:25:29 +0000

    POD fixes
    svn path=/bioperl-run/trunk/; revision=13229

--- Bio/Tools/Run/Prints.pm
+++ Bio/Tools/Run/Prints.pm
@@ -1,10 +1,65 @@
+# Copyright Balamurugan Kumarasamy
+#
+# You may distribute this module under the same terms as perl itself
+# POD documentation - main docs before the code
+
+=head1 NAME
+
+Bio::Tools::Run::Prints 
+
+=head1 SYNOPSIS
+
+  Build a Prints factory
+
+  # $paramfile is the full path to the Prints binary file
+  my @params = ('DB',$dbfile,'PROGRAM',$paramfile);
+  my $factory = Bio::Tools::Run::Prints->new($params);
+
+  # Pass the factory a Bio::Seq object
+  # @feats is an array of Bio::SeqFeature::Generic objects
+  my @feats = $factory->predict_protein_features($seq);
+
+=head1 DESCRIPTION
+
+  PRINTScan II is a PRINTS fingerprint identification algorithm.
+  Copyright (C) 1998,1999  Phil Scordis
+=head1 FEEDBACK
+
+=head2 Mailing Lists
+
+ User feedback is an integral part of the evolution of this and other
+ Bioperl modules. Send your comments and suggestions preferably to one
+ of the Bioperl mailing lists.  Your participation is much appreciated.
+
+ bioperl-l@bioperl.org          - General discussion
+ http://bio.perl.org/MailList.html             - About the mailing lists
+
+=head2 Reporting Bugs
+
+ Report bugs to the Bioperl bug tracking system to help us keep track
+ the bugs and their resolution.  Bug reports can be submitted via
+ email or the web:
+
+ bioperl-bugs@bio.perl.org
+ http://bio.perl.org/bioperl-bugs/
+
+=head1 AUTHOR - Bala
+
+ Email savikalpa@fugu-sg.org
+
+=head1 APPENDIX
+
+ The rest of the documentation details each of the object
+ methods. Internal methods are usually preceded with a _
+
+=cut
+
 package Bio::Tools::Run::Prints;
 
 use vars qw($AUTOLOAD @ISA $PROGRAM  $PROGRAMDIR
             $TMPDIR $PROGRAMNAME @PRINTS_PARAMS
                          %OK_FIELD);
 use strict;
-use Bio::Seq;
 use Bio::SeqIO;
 use Bio::Root::Root;
 use Bio::Root::IO;
@@ -29,7 +84,6 @@ BEGIN {
           $PROGRAM = 'Prints';
        }
       
-       $TMPDIR=Bio::Root::IO->catfile("","/scratch/bala");
        @PRINTS_PARAMS=qw(DB PROGRAM VERBOSE);
        foreach my $attr ( @PRINTS_PARAMS)
                         { $OK_FIELD{$attr}++; }
@@ -45,13 +99,22 @@ sub AUTOLOAD {
        return $self->{$attr};
 }
 
+=head2 new
+
+ Title   : new
+ Usage   : $prints->new(@params)
+ Function: creates a new Prints factory
+ Returns:  Bio::Tools::Run::Prints
+ Args    :
+
+=cut
+
 sub new {
        my ($class,@args) = @_;
        my $self = $class->SUPER::new(@args);
        $self->io->_initialize_io();
  
        my ($attr, $value);
-       #($TMPDIR) = $self->tempdir(CLEANUP=>1);
        while (@args)  {
            $attr =   shift @args;
            $value =  shift @args;
@@ -67,11 +130,11 @@ sub new {
 
 =head2 executable
 
-Title   : executable
-Usage   : my $exe = $genscan->executable();
-Function: Finds the full path to the 'genscan' executable
-Returns : string representing the full path to the exe
-Args    : [optional] name of executable to set path to
+ Title   : executable
+ Usage   : my $exe = $prints->executable();
+ Function: Finds the full path to the Prints executable
+ Returns : string representing the full path to the exe
+ Args    : [optional] name of executable to set path to
           [optional] boolean flag whether or not warn when exe is not found
 
 
@@ -98,32 +161,42 @@ sub executable{
             }
         }
     }
-      $self->{'_pathtoexe'};
+      return $self->{'_pathtoexe'};
 }
+
+=head2 predict_protein_features
+
+ Title   :   predict_protein_features()
+ Usage   :   $obj->predict_protein_features($seqFile)
+ Function:   Runs Prints and creates an array of featrues
+ Returns :   An array of Bio::SeqFeature::Generic objects
+ Args    :   A Bio::PrimarySeqI
+
+=cut
 
 sub predict_protein_features{
     my ($self,$seq) = @_;
     my @feats;
-    eval {
-       $seq->isa ("Bio::PrimarySeqI") || $seq->isa ("Bio::SeqI")
-    };
 
-
-    if (!$@) {
+    if  (ref($seq) ){# it is an object
+        
+        if (ref($seq) =~ /GLOB/) {
+           $self->throw("cannot use filehandle");
+        }
     
         my $infile1 = $self->_writeSeqFile($seq);
         
-        $self->_set_input($infile1);
+        $self->_input($infile1);
         
-         @feats = $self->_run();
-         my $file_no = unlink $infile1;
+        @feats = $self->_run();
+         unlink $infile1;
     }
     else {
         #The clone object is not a seq object but a file.
         #Perhaps should check here or before if this file is fasta format...if not die
         #Here the file does not need to be created or deleted. Its already written and may be used by other runnables.
 
-        $self->_set_input($seq);
+        $self->_input($seq);
 
          @feats = $self->_run();
     }
@@ -132,21 +205,45 @@ sub predict_protein_features{
 
 }
 
-sub _set_input() {
+=head2 _input
+
+ Title   :   _input
+ Usage   :   obj->_input($seqFile)
+ Function:   Internal(not to be used directly)
+ Returns :
+ Args    :
+
+=cut
+
+sub _input() {
      my ($self,$infile1) = @_;
-     $self->{'input'}=$infile1;
+     
+     if(defined $infile1){
+         
+        $self->{'input'}=$infile1;
+     }   
+     return $self->{'input'};
+
 }
+
+=head2 _run
+
+ Title   :   _run
+ Usage   :   $obj->_run()
+ Function:   Internal(not to be used directly)
+ Returns :   An array of Bio::SeqFeature::Generic objects
+ Args    :
+
+=cut
 
 sub _run {
      my ($self)= @_;
      
-     my ($tfh1,$outfile) = $self->io->tempfile(-dir=>$TMPDIR);
-      my $str =$self->{'_pathtoexe'}." ".$self->DB." ".$self->{'input'}." -fjR >".$outfile;
-     print STDERR "$str\n";
+     my ($tfh1,$outfile) = $self->io->tempfile(-dir=>$self->tempdir());
+      my $str =$self->executable." ".$self->DB." ".$self->_input." -fjR >".$outfile;
      my $status = system($str);
      $self->throw( "Prints call ($str) crashed: $? \n") unless $status==0;
      
-    # my $prints_parser = Bio::Tools::Prints->new();
      my $filehandle;
      if (ref ($outfile) !~ /GLOB/) {
         open (PRINTS, "<".$outfile) or $self->throw ("Couldn't open file ".$outfile.": $!\n");
@@ -164,16 +261,25 @@ sub _run {
      
      
      $self->cleanup();
-     my $file_no = unlink $outfile;    
+     unlink $outfile;    
      return @prints_feat;
      
 }
 
 
+=head2 _writeSeqFile
+
+ Title   :   _writeSeqFile
+ Usage   :   obj->_writeSeqFile($seq)
+ Function:   Internal(not to be used directly)
+ Returns :
+ Args    :
+
+=cut
+
 sub _writeSeqFile{
     my ($self,$seq) = @_;
-    #my $tmpdir = $self->io->tempdir(CLEANUP=>1);
-    my ($tfh,$inputfile) = $self->io->tempfile(-dir=>$TMPDIR);
+    my ($tfh,$inputfile) = $self->io->tempfile(-dir=>$self->tempdir());
     my $in  = Bio::SeqIO->new(-fh => $tfh , '-format' => 'Fasta');
     $in->write_seq($seq);
 
