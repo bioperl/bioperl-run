b8285f35ffbe46e35819598caeca3c9107c4378e
avilella <avilella@bioperl.org>
2004-03-10 14:14:09 +0000

    added
    svn path=/bioperl-run/trunk/; revision=13526

---
+++ Bio/Installer/EMBOSS.pm
@@ -0,0 +1,200 @@
+
+# BioPerl module for Bio::Installer::EMBOSS
+#
+# Cared for by Albert Vilella <avilella@ebi.ac.uk, avilella@ub.edu>
+#
+# Copyright Albert Vilella
+#
+# You may distribute this module under the same terms as perl itself
+
+# POD documentation - main docs before the code
+
+=head1 NAME
+
+Bio::Installer::EMBOSS - DESCRIPTION of Object
+
+=head1 SYNOPSIS
+
+Give standard usage here
+
+=head1 DESCRIPTION
+
+Describe the object here
+
+=head1 FEEDBACK
+
+=head2 Mailing Lists
+
+User feedback is an integral part of the evolution of this and other
+Bioperl modules. Send your comments and suggestions preferably to
+the Bioperl mailing list.  Your participation is much appreciated.
+
+  bioperl-l@bioperl.org              - General discussion
+  http://bioperl.org/MailList.shtml  - About the mailing lists
+
+=head2 Reporting Bugs
+
+Report bugs to the Bioperl bug tracking system to help us keep track
+of the bugs and their resolution. Bug reports can be submitted via
+the web:
+
+  http://bugzilla.bioperl.org/
+
+=head1 AUTHOR - Albert Vilella
+
+Email avilella@ebi.ac.uk, avilella@ub.edu
+
+Describe contact details here
+
+=head1 CONTRIBUTORS
+
+Additional contributors names and emails here
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object methods.
+Internal methods are usually preceded with a _
+
+=cut
+
+
+# Let the code begin...
+
+
+package Bio::Installer::EMBOSS;
+use vars qw(@ISA %DEFAULTS);
+use strict;
+
+# Object preamble - inherits from Bio::Root::Root
+
+use Bio::Root::Root;
+use Bio::Installer::Generic;
+
+@ISA = qw(Bio::Installer::Generic );
+
+BEGIN {
+    %DEFAULTS = 
+        ( 'ORIGIN_DOWNLOAD_DIR' => 'ftp://ftp.uk.embnet.org/pub/EMBOSS/',
+          'DESTINATION_DOWNLOAD_DIR' => '/tmp',
+          'DESTINATION_INSTALL_DIR' => "/usr/local",
+          'PACKAGE_NAME' => 'EMBOSS-2.8.0.tar.gz',
+          'DIRECTORY_NAME' => 'EMBOSS-2.8.0'
+        );
+}
+
+
+=head2 get_default 
+
+ Title   : get_default 
+ Usage   :
+ Function:
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub get_default {
+    my $self = shift;
+    my $param = shift;
+    return $DEFAULTS{$param};
+}
+
+
+=head2 install
+
+ Title   : install
+ Usage   : $installer->install();
+ Function:
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub install{
+   my ($self,@args) = @_;
+   $self->_decompress;
+   $self->_execute_typical_nix_install;
+   $self->_remember_env_is_in_path;
+}
+
+
+=head2 _execute_typical_nix_install
+
+ Title   : _execute_typical_nix_install
+ Usage   :
+ Function:
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub _execute_typical_nix_install{
+   my ($self,@args) = @_;
+   my $call;
+
+   my $destination = $self->destination_install_dir . "/" . $self->directory_name;
+
+   print "\n\nTypical linux install -- configure -- make -- make install (this might take a while)\n\n";sleep 1;
+   if (($^O =~ /dec_osf|linux|unix|bsd|solaris|darwin/i)) {
+       chdir $destination or die "Cant cd to $destination $!\n";
+
+       print "\n\nCalling configure (this might take a while)\n\n";sleep 1;
+       $call = "./configure --prefix=$destination";
+       system("$call") == 0 or $self->throw("Error when trying to run configure");
+
+       print "\n\nCalling make (this might take a while)\n\n";sleep 1;
+       $call = "make";
+       system("$call") == 0 or $self->throw("Error when trying to run make");
+       print "\n\nCalling make install (this might take a while)\n\n";sleep 1;
+       if (-w "$destination") {
+           $call = "make install";
+       } else {
+           print "\n\n You may need administrative password to install\n"
+               ."this program so that bioperl can see it in the binary PATH\n";
+           $call = "su -c \"make install\"";
+       }
+       system("$call") == 0 or $self->throw("Error when trying to run make install");
+
+   } else {
+       $self->throw("_execute_typical_nix not yet implemented in this platform");
+   }
+}
+
+
+=head2 _remember_env_is_in_path
+
+ Title   : _remember_env_is_in_path
+ Usage   :
+ Function:
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub _remember_env_is_in_path{
+   my ($self,@args) = @_;
+   my $dir;
+   $dir = $self->destination_install_dir;
+   unless ($dir =~ m|/usr/local|) {
+       print STDERR <<END;
+
+You may need to make $dir visible for \$PATH to help bioperl find the
+program. This can be done with a symbolic link (as root):
+
+    ln -s $dir/bin /usr/local/bin
+END
+  ;
+}
+
+}
+
+
+1;
---
+++ Bio/Installer/PAML.pm
@@ -0,0 +1,199 @@
+#!/usr/local/bin/perl
+
+# BioPerl module for Bio::Installer::PAML
+#
+# Cared for by Albert Vilella <avilella@ebi.ac.uk, avilella@ub.edu>
+#
+# Copyright Albert Vilella
+#
+# You may distribute this module under the same terms as perl itself
+
+# POD documentation - main docs before the code
+
+=head1 NAME
+
+Bio::Installer::PAML - DESCRIPTION of Object
+
+=head1 SYNOPSIS
+
+Give standard usage here
+
+=head1 DESCRIPTION
+
+Describe the object here
+
+=head1 FEEDBACK
+
+=head2 Mailing Lists
+
+User feedback is an integral part of the evolution of this and other
+Bioperl modules. Send your comments and suggestions preferably to
+the Bioperl mailing list.  Your participation is much appreciated.
+
+  bioperl-l@bioperl.org              - General discussion
+  http://bioperl.org/MailList.shtml  - About the mailing lists
+
+=head2 Reporting Bugs
+
+Report bugs to the Bioperl bug tracking system to help us keep track
+of the bugs and their resolution. Bug reports can be submitted via
+the web:
+
+  http://bugzilla.bioperl.org/
+
+=head1 AUTHOR - Albert Vilella
+
+Email avilella@ebi.ac.uk, avilella@ub.edu
+
+Describe contact details here
+
+=head1 CONTRIBUTORS
+
+Additional contributors names and emails here
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object methods.
+Internal methods are usually preceded with a _
+
+=cut
+
+
+# Let the code begin...
+
+
+package Bio::Installer::PAML;
+use vars qw(@ISA %DEFAULTS);
+use strict;
+
+# Object preamble - inherits from Bio::Root::Root
+
+use Bio::Root::Root;
+use Bio::Installer::Generic;
+
+@ISA = qw(Bio::Installer::Generic );
+
+BEGIN {
+    %DEFAULTS = ( 'ORIGIN_DOWNLOAD_DIR' => 'ftp://abacus.gene.ucl.ac.uk/pub/paml/',
+                  'BIN_FOLDER' => 'src',
+                  'DESTINATION_DOWNLOAD_DIR' => '/tmp',
+                  'DESTINATION_INSTALL_DIR' => "$ENV{'HOME'}",
+                  'PACKAGE_NAME' => 'paml3.13d.tar.gz',
+                  'DIRECTORY_NAME' => 'paml3.13d',
+                  'ENV_NAME' => 'PAMLDIR',
+                );
+}
+
+
+=head2 get_default 
+
+ Title   : get_default 
+ Usage   :
+ Function:
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub get_default {
+    my $self = shift;
+    my $param = shift;
+    return $DEFAULTS{$param};
+}
+
+
+=head2 install
+
+ Title   : install
+ Usage   : $installer->install();
+ Function:
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub install{
+   my ($self,@args) = @_;
+   $self->_decompress;
+   $self->_tweak_paml_makefile;
+   $self->_execute_paml_makefile;
+   $self->_remember_env;
+}
+
+
+=head2 _execute_paml_makefile
+
+ Title   : _execute_paml_makefile
+ Usage   :
+ Function:
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub _execute_paml_makefile{
+   my ($self,@args) = @_;
+   my $call;
+
+   my $destination = $self->destination_install_dir . "/" . $self->directory_name . "/src";
+   chdir $destination or $self->throw("Cant cd to $destination");
+   print "\n\nCompiling now... (this might take a while)\n\n";
+   if (($^O =~ /dec_osf|linux|unix|bsd|solaris|darwin/i)) {
+       $call = "make";
+       system("$call") == 0 or $self->throw("Error when trying to run make");
+   } else {
+       $self->throw("_execute_paml_makefile not yet implemented in this platform");
+   }
+}
+
+
+=head2 _tweak_paml_makefile
+
+ Title   : _tweak_paml_makefile
+ Usage   :
+ Function:
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub _tweak_paml_makefile{
+    my ($self,@args) = @_;
+    my $gcc3;
+    my $return = qx/gcc -v 2>&1/;
+    if ($return =~ /version 3/) {
+        $return = `cat /proc/cpuinfo`;
+        if( ($return =~ /mmx/) || ($return =~ /3dnow/)) {
+            my $destination = $self->destination_install_dir . "/" . $self->directory_name . "/src";
+            chdir $destination or $self->throw("Cant cd to $destination");
+            my $new = "Makefile";
+            open(OLD, "< Makefile.UNIX")         or  $self->throw("can't open Makefile.UNIX");
+            open(NEW, "> $new")         or $self->throw("can't open $new");
+            while (<OLD>) {
+                # change $_, then...
+                $_ =~ s/CFLAGS = -O3/\# CFLAGS = -O3/g;
+                if( $return =~ /3dnow/ ) {
+                    $_ =~ s/#CFLAGS = -march=athlon -mcpu=athlon -O4 -funroll-loops -fomit-frame-pointer -finline-functions/CFLAGS = -march=athlon -mcpu=athlon -O4 -funroll-loops -fomit-frame-pointer -finline-functions/g;
+                } elsif( $return =~ /mmx/ ) {
+                    $_ =~ s/#CFLAGS = -march=pentiumpro -mcpu=pentiumpro -O4 -funroll-loops -fomit-frame-pointer -finline-functions/CFLAGS = -march=pentiumpro -mcpu=pentiumpro -O4 -funroll-loops -fomit-frame-pointer -finline-functions/g;
+                }
+                print NEW $_ or $self->throw("can't write $new");
+            }
+            close(OLD)                  or $self->throw("can't close Makefile.UNIX");
+            close(NEW)                  or $self->throw("can't close $new");
+        }
+    }
+
+
+}
+
+
+1;
