ce05c5301292ab5346ae1e8abba2e5b09de23661
sendu <sendu@bioperl.org>
2007-02-15 13:39:36 +0000

    reformulated for better generality
    svn path=/bioperl-run/trunk/; revision=13889

--- Bio/Tools/Run/Phylo/PhyloBase.pm
+++ Bio/Tools/Run/Phylo/PhyloBase.pm
@@ -59,110 +59,134 @@ Internal methods are usually preceded with a _
 package Bio::Tools::Run::Phylo::PhyloBase;
 use strict;
 
-use Cwd;
-use File::Spec;
 use Bio::AlignIO;
 use Bio::TreeIO;
 
 use base qw(Bio::Tools::Run::WrapperBase);
 
-=head2 _writeAlignFile
 
- Title   : _writeAlignFile
- Usage   : $obj->_writeAlignFile($aln)
- Function: Writes the supplied alignment object out in the desired format to
-           a temp file.
- Returns : n/a (sets _alignment_file() and _alignment_object())
- Args    : Bio::Align::AlignI AND format (default 'fasta');
+=head2 _alignment
+
+ Title   : _alignment
+ Usage   : $aln = $obj->_alignment()
+ Function: Get/set an alignment object, generating one from a file if desired.
+ Returns : Bio::Align::AlignI (probably a Bio::SimpleAlign)
+ Args    : none to get
+           OR filename & input format of the alignment file (latter defaults to
+           guess) to set from file
+           OR Bio::Align::AlignI to set
 
 =cut
 
-sub _writeAlignFile {
-    my ($self, $align, $format) = @_;
-    $self->_alignment_object($align);
+sub _alignment {
+    my ($self, $thing, $format) = @_;
+    
+    if (ref($thing) && $thing->isa('Bio::Align::AlignI')) {
+        $self->{_align_obj} = $thing;
+    }
+    elsif ($thing && -e $thing) {
+        my $align_in = Bio::AlignIO->new(-verbose => $self->verbose, -file => $thing, $format ? (-format => $format) : ());
+        my $aln = $align_in->next_aln || $self->throw("Alignment file '$thing' had no alignment!");
+        $self->{_align_obj} = $aln;
+    }
+    
+    return $self->{_align_obj};
+}
+
+=head2 _write_alignment
+
+ Title   : _write_alignment
+ Usage   : $obj->_write_alignment()
+ Function: Writes the alignment object returned by _alignment() out in the
+           desired format to a temp file.
+ Returns : filename
+ Args    : string to desribe format (default 'fasta')
+
+=cut
+
+sub _write_alignment {
+    my ($self, $format) = @_;
+    my $align = $self->_alignment || $self->throw("_write_alignment called when _alignment had not been set");
     $format ||= 'fasta';
     
-    my ($tfh, $tempfile) = $self->io->tempfile(-dir=>$self->tempdir);
+    my ($tfh, $tempfile) = $self->io->tempfile(-dir => $self->tempdir);
     
     my $out = Bio::AlignIO->new(-verbose => $self->verbose, '-fh' => $tfh, '-format' => $format);
     $align->set_displayname_flat;
-    $self->_alignment_id($align->id || $tempfile);
     $out->write_aln($align);
     
     $out->close();
     $out = undef;
     close($tfh);
     undef $tfh;
-    $self->_alignment_file($tempfile);
+    
+    return $tempfile;
 }
 
-=head2 _writeTreeFile
+=head2 _tree
 
- Title   : _writeTreeFile
- Usage   : obj->_writeTreeFile($tree)
- Function: Writes the supplied tree object out in the desired format to a temp
-           file.
- Returns : n/a (sets _tree_file())
- Args    : Bio::Tree::TreeI OR Bio::DB::Taxonomy (_alignment_file() must be
-           set in the latter case, where the corresponding alignment file has
-           sequences with ids matching species in the taxonomy database)
-
-           Optionally, any of these key => value pairs:
-           format => string # desired output format, default 'newick'
-           binary => 1      # force the tree that is output to be binary
-           unquoted => 1    # do not allow the output tree to have quoted
-                              strings; converts spaces in ids to underscores
+ Title   : _tree
+ Usage   : $tree = $obj->_tree()
+ Function: Get/set a tree object, generating one from a file/database if desired
+ Returns : Bio::Tree::TreeI
+ Args    : none to get, OR to set:
+           OR filename & input format of the tree file (latter defaults to
+           guess) to set from file
+           OR Bio::Tree::TreeI
+           OR Bio::DB::Taxonomy when _alignment() has been set and where
+           sequences in the alignment have ids matching species in the taxonomy
+           database
 
 =cut
 
-sub _writeTreeFile {
-    my ($self, $thing, @args) = @_;
+sub _tree {
+    my ($self, $thing, $format) = @_;
     
-    my ($format, $binary, $unquoted) = $self->_rearrange([qw(FORMAT
-                                                             BINARY
-                                                             UNQUOTED)], @args);
-    $format ||= 'newick';
-    
-    my $tree;
-    if (-e $thing) {
-        # read the tree in so we can later spit it out in desired format
-        my $in = Bio::TreeIO->new(-verbose => $self->verbose, -file => $thing, -format => $format);
-        $tree = $in->next_tree;
-    }
-    elsif ($thing->isa('Bio::Tree::TreeI')) {
-        $tree = $thing;
-    }
-    elsif ($thing->isa('Bio::DB::Taxonomy')) {
-        $self->throw("When supplying a Bio::DB::Taxonomy, _alignment_file() must first be set") unless $self->_alignment_file;
-        
-        # get all the alignment sequence names
-        my @species_names = $self->_get_seq_names;
-        
-        $tree = $thing->get_tree(@species_names);
-        
-        # convert node ids to their seq_ids for correct output with TreeIO
-        foreach my $node ($tree->get_nodes) {
-            my $seq_id = $node->name('supplied');
-            $seq_id = $seq_id ? shift @{$seq_id} : ($node->node_name ? $node->node_name : $node->id);
+    if ($thing) {
+        my $tree;
+        if (ref($thing) && $thing->isa('Bio::Tree::TreeI')) {
+            $tree = $thing;
+        }
+        elsif (ref($thing) && $thing->isa('Bio::DB::Taxonomy')) {
+            # get all the alignment sequence names
+            my @species_names = $self->_get_seq_names;
             
-            $node->id($seq_id);
+            $tree = $thing->get_tree(@species_names);
+            
+            # convert node ids to their seq_ids for correct output with TreeIO
+            foreach my $node ($tree->get_nodes) {
+                my $seq_id = $node->name('supplied');
+                $seq_id = $seq_id ? shift @{$seq_id} : ($node->node_name ? $node->node_name : $node->id);
+                
+                $node->id($seq_id);
+            }
         }
-    }
-    else {
-        $self->throw("Unknown input type [$thing]");
+        elsif (-e $thing) {
+            my $tree_in = Bio::TreeIO->new(-verbose => $self->verbose, -file => $thing, $format ? (-format => $format) : ());
+            $tree = $tree_in->next_tree || $self->throw("Tree file '$thing' had no tree!");
+        }
+        
+        $self->{_tree_obj} = $tree || $self->throw("'$thing' supplied but unable to generate a tree from it");
     }
     
-    # force the tree to be binary
-    $tree->force_binary if $binary;
-    
-    # get rid of spaces in ids to prevent quoted strings
-    if ($unquoted) {
-        foreach my $node ($tree->get_nodes) {
-            my $id = $node->id;
-            $id =~ s/ /_/g;
-            $node->id($id);
-        }
-    }
+    return $self->{_tree_obj};
+}
+
+=head2 _write_tree
+
+ Title   : _write_tree
+ Usage   : $obj->_write_tree()
+ Function: Writes the tree object returned by _tree() out in the desired format
+           to a temp file.
+ Returns : filename
+ Args    : string to desribe format (default 'newick')
+
+=cut
+
+sub _write_tree {
+    my ($self, $format) = @_;
+    my $tree = $self->_tree || $self->throw("_write_tree called when _tree had not been set");
+    $format ||= 'newick';
     
     my ($tfh, $tempfile) = $self->io->tempfile(-dir => $self->tempdir);
     
@@ -173,57 +197,8 @@ sub _writeTreeFile {
     $out = undef;
     close($tfh);
     undef $tfh;
-    $self->_tree_file($tempfile);
-}
-
-=head2 _alignment_file
-
- Title   : _alignment_file
- Usage   : $obj->_alignment_file($str)
- Function: Get/set location of alignment file program being wrapped will operate
-           on. When getting, the location is the absolute path to the file.
- Returns : string (file path, absolute)
- Args    : string (file path, can be relative)
-
-=cut
-
-sub _alignment_file {
-    my $self = shift;
-    if (@_) { $self->{_align_file} = Cwd::abs_path(shift) }
-    return $self->{_align_file} || '';
-}
-
-=head2 _alignment_id
-
- Title   : _alignment_id
- Usage   : $obj->_alignment_id($str)
- Function: Get/set alignment id. Typically set by _writeAlignFile().
- Returns : string
- Args    : none to get, string to set
-
-=cut
-
-sub _alignment_id {
-    my $self = shift;
-    if (@_) { $self->{_alignment_id} = shift }
-    return $self->{_alignment_id} || '';
-}
-
-=head2 _tree_file
-
- Title   : _tree_file
- Usage   : $obj->_tree_file($str)
- Function: Get/set location of tree file program being wrapped will operate on
-           When getting, the location is the absolute path to the file.
- Returns : string (file path, absolute)
- Args    : string (file path, can be relative)
-
-=cut
-
-sub _tree_file {
-    my $self = shift;
-    if (@_) { $self->{_tree_file} = Cwd::abs_path(shift) }
-    return $self->{_tree_file} || '';
+    
+    return $tempfile;
 }
 
 =head2 _get_seq_names
@@ -231,15 +206,15 @@ sub _tree_file {
  Title   : _get_seq_names
  Usage   : @names = $obj->_get_seq_names()
  Function: Get all the sequence names (from id()) of the sequenes in the
-           alignment.  _alignment_object() must be set prior to calling this.
+           alignment.  _alignment() must be set prior to calling this.
  Returns : list of strings (seq ids)
- Args    : input format of the alignment file (defaults to guess)
+ Args    : none
 
 =cut
 
 sub _get_seq_names {
     my $self = shift;
-    my $aln = $self->_alignment_object(@_);
+    my $aln = $self->_alignment || $self->throw("_get_seq_names called when _alignment had not been set");
     
     my @names;
     foreach my $seq ($aln->each_seq) {
@@ -249,58 +224,20 @@ sub _get_seq_names {
     return @names;
 }
 
-=head2 _alignment_object
-
- Title   : _alignment_object
- Usage   : $aln = $obj->_alignment_object()
- Function: Read the alignment file and generate an alignment object from it.
-           _alignment_file() must be set prior to calling this.
- Returns : Bio::SimpleAlign
- Args    : none or input format of the alignment file (defaults to guess) to
-           get, or Bio::Align::AlignI to set.
-
-=cut
-
-sub _alignment_object {
-    my $self = shift;
-    
-    if (defined $self->{_align_obj}) {
-        return $self->{_align_obj};
-    }
-    
-    my $thing = shift;
-    if (ref($thing) && $thing->isa('Bio::Align::AlignI')) {
-        $self->{_align_obj} = $thing;
-        return $thing;
-    }
-    my $format = $thing;
-    
-    my $file = $self->_alignment_file || $self->throw("Alignment filename hasn't been set with _alignment_file");
-    
-    my $align_in = Bio::AlignIO->new(-verbose => $self->verbose, -file => $file, $format ? (-format => $format) : ());
-    my $aln = $align_in->next_aln || $self->throw("Alignment file '$file' had no alignment!");
-    $self->{_align_obj} = $aln;
-    return $aln;
-}
-
 =head2 _get_node_names
 
  Title   : _get_node_names
  Usage   : @names = $obj->_get_node_names()
  Function: Get all the node names (from id()) of the nodes in the tree.
-           _tree_file() must be set prior to calling this.
+           _tree must be set prior to calling this.
  Returns : list of strings (node ids)
- Args    : input format of the tree file (defaults to guess)
+ Args    : none
 
 =cut
 
 sub _get_node_names {
     my $self = shift;
-    my $file = $self->_tree_file || $self->throw("Tree filename hasn't been set");
-    my $format = shift;
-    
-    my $tree_in = Bio::TreeIO->new(-verbose => $self->verbose, -file => $file, $format ? (-format => $format) : ());
-    my $tree = $tree_in->next_tree || $self->throw("Tree file '$file' had no tree!");
+    my $tree = $self->_tree || $self->throw("_get_node_names called when _tree had not been set");
     
     my @names;
     foreach my $node ($tree->get_leaf_nodes) {
@@ -315,19 +252,19 @@ sub _get_node_names {
  Title   : _check_names
  Usage   : if ($obj->_check_names) { ... }
  Function: Determine if all sequences in the alignment file have a corresponding
-           node in the tree file. _alignment_file() and _tree_file() must be set
+           node in the tree file. _alignment() and _tree() must be set
            prior to calling this.
  Returns : boolean (will also warn about the specific problems when returning
            false)
- Args    : input format of the alignment and tree files (both default to guess)
+ Args    : none
 
 =cut
 
 sub _check_names {
-    my ($self, $aformat, $tformat) = @_;
+    my $self = shift;
     
-    my @seq_names = $self->_get_seq_names($aformat);
-    my %node_names = map { $_ => 1 } $self->_get_node_names($tformat);
+    my @seq_names = $self->_get_seq_names;
+    my %node_names = map { $_ => 1 } $self->_get_node_names;
     
     # (not interested in tree nodes that don't map to sequence, since we
     #  expect the tree to have internal nodes not represented by sequence)
@@ -341,6 +278,7 @@ sub _check_names {
         $self->warn("$count unmapped ids between the supplied alignment and tree: $unmapped");
         return 0;
     }
+    
     return 1;
 }
 
