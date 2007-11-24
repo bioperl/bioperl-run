a7bf5d19a6971dfefe8ddb0f3b6200c2cdb21d91
avilella <avilella@bioperl.org>
2007-11-24 11:12:02 +0000

    updated to latest server and versino
    svn path=/bioperl-run/trunk/; revision=13951

--- Bio/Installer/TCoffee.pm
+++ Bio/Installer/TCoffee.pm
@@ -73,12 +73,14 @@ use Bio::Installer::Generic;
 @ISA = qw(Bio::Installer::Generic );
 
 BEGIN {
-    %DEFAULTS = ( 'ORIGIN_DOWNLOAD_DIR' => 'http://igs-server.cnrs-mrs.fr/~cnotred/Packages',
+    %DEFAULTS = ( 'ORIGIN_DOWNLOAD_DIR' => 'http://www.tcoffee.org/Packages/',
                   'BIN_FOLDER' => 'bin',
                   'DESTINATION_DOWNLOAD_DIR' => '/tmp',
                   'DESTINATION_INSTALL_DIR' => "$ENV{'HOME'}",
-                  'PACKAGE_NAME' => 'T-COFFEE_distribution_Version_4.45.tar.gz',
-                  'DIRECTORY_NAME' => 'T-COFFEE_distribution_Version_4.45',
+#                  'PACKAGE_NAME' => 'T-COFFEE_distribution_Version_4.45.tar.gz',
+                  'PACKAGE_NAME' => 'T-COFFEE_distribution_Version_5.37.tar.gz',
+#                  'DIRECTORY_NAME' => 'T-COFFEE_distribution_Version_4.45',
+                  'DIRECTORY_NAME' => 'T-COFFEE_distribution_Version_5.37',
                   'ENV_NAME' => 'TCOFFEEDIR',
                 );
 }
