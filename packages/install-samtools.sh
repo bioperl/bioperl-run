697cd5378f8ae3217fd0ce3036368e17304af61f
Chris Fields <cjfields@illinois.edu>
2016-12-18 23:35:26 -0600

    retry non-sudo containerized approach

--- packages/install-samtools.sh
+++ packages/install-samtools.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget https://github.com/samtools/samtools/releases/download/1.3.1/samtools-1.3.1.tar.bz2
 tar -xjvf samtools-1.3.1.tar.bz2
-cd samtools-1.3.1 && make && sudo make install
+cd samtools-1.3.1 && make prefix=$HOME/local && make prefix=$HOME/local install
